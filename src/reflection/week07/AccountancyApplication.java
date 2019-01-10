9305322940e4f168dcccc7f15a83f31f17adc42a
DIBA <diba.kheirkhah@gmail.com>
2019-01-10 23:47:37 +0100

    reflection about FileReader, traits and classes

---
+++ src/reflection/week07/AccountancyApplication.java
@@ -0,0 +1,11 @@
+package reflection.week07;
+
+public class AccountancyApplication {
+    public static void main(String[] args) {
+
+        PaymentAutomation paymentAutomation = new PaymentAutomation();
+        paymentAutomation.produceStatistic();
+
+
+    }
+}
---
+++ src/reflection/week07/PaymentAutomation.java
@@ -0,0 +1,34 @@
+package reflection.week07;
+
+import java.util.List;
+
+public class PaymentAutomation {
+    private DataGatherer dataGatherer = new DataGatherer();
+
+
+    public void produceStatistic() {
+        List<Employee> employees = dataGatherer.asList();
+
+        showTransfers(employees);
+
+
+        ProduceStatistics statistics = new ProduceStatistics();
+        Integer totalPeyment = statistics.calculateTotalPayment(employees);
+        System.out.println("Total amount paid: " + totalPeyment+"€");
+
+        String highestSalaryDepartment = statistics.calculateHighestSalaryDepartment(employees);
+        System.out.println("Most paying department: " + highestSalaryDepartment);
+
+         statistics.findHighestSalary(employees);
+
+
+
+    }
+
+
+    private void showTransfers(List<Employee> employees) {
+        for (Employee employee : employees) {
+            System.out.println("Transfer successful: " + employee.getSalary() + "euro to account " + employee.getAccount());
+        }
+    }
+}
