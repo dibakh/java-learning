340232f9d3bf777d1e73dba2a6821b7cbab2a3d1
DIBA <diba.kheirkhah@gmail.com>
2019-01-11 12:17:16 +0100

    filtering odd and even numbers in a list

---
+++ src/exercises/week08/ex02/NumberFilterTest.java
@@ -0,0 +1,37 @@
+package exercises.week08.ex02;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class NumberFilterTest {
+    private NumberFilter numberFilter = new NumberFilter ( );
+
+    private static List<Integer> getNumbers() {
+        List<Integer> numbers = new ArrayList<> (Arrays.asList (1, 2, 3, 4, 5, 6, 7, 8, 9));
+        return numbers;
+    }
+
+    private List<Integer> numbers = getNumbers ( );
+
+    @Test
+    public void getEven() {
+        List<Integer> answer = numberFilter.getEven (numbers);
+        List<Integer> expected = Arrays.asList (2, 4, 6, 8);
+
+        Assertions.assertEquals(expected,answer);
+    }
+
+    @Test
+    public void getOdd() {
+
+        List<Integer> answer = numberFilter.getOdd (numbers);
+        List<Integer> expected = Arrays.asList (1, 3, 5, 7, 9);
+
+        Assertions.assertEquals(expected,answer);
+    }
+}
