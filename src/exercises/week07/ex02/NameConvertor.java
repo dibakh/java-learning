81aac8e06c027da1e11c9b22afec31a6389024fb
DIBA <diba.kheirkhah@gmail.com>
2019-01-14 05:58:48 +0100

    creating objects from classes and call them

---
+++ src/exercises/week07/ex02/NameConvertor.java
@@ -0,0 +1,19 @@
+package exercises.week07.ex02;
+
+import java.util.List;
+
+public class NameConvertor {
+    public void convertNameToNickName() {
+
+        NameGartherer nickNameGatherer = new NameGartherer();
+        NickNameCreator nickNameCreator = new NickNameCreator();
+
+        List<String> names = nickNameGatherer.gather();
+        List<String> nickNames = nickNameCreator.creator(names);
+        nickNameCreator.display(nickNames);
+
+
+
+    }
+
+}
