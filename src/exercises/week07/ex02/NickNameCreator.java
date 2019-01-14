914658d4a4a47f667bd50962b43b6dc70dc32f37
DIBA <diba.kheirkhah@gmail.com>
2019-01-14 06:00:19 +0100

    convert a name to nick name

---
+++ src/exercises/week07/ex02/NickNameCreator.java
@@ -0,0 +1,31 @@
+package exercises.week07.ex02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NickNameCreator {
+    private List<String> nickNames = new ArrayList<>();
+
+    public List<String> creator(List<String> names) {
+
+        for (String name : names) {
+            int lengthOfName = name.length();
+
+            if (lengthOfName == 1) {
+                nickNames.add(name);
+            } else {
+
+                String nickName = name.substring(0, lengthOfName / 2);
+                nickNames.add(nickName);
+            }
+        }
+
+        return  nickNames;
+    }
+
+    public void display(List<String> nickNames) {
+        System.out.println("nickName : " + nickNames);
+    }
+}
+
+
