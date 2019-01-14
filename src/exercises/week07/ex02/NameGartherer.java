767b04fdc5a0b46694431632459b1a29e3f192f4
DIBA <diba.kheirkhah@gmail.com>
2019-01-14 05:55:19 +0100

    read names and add them to list

---
+++ src/exercises/week07/ex02/NameGartherer.java
@@ -0,0 +1,27 @@
+package exercises.week07.ex02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NameGartherer {
+    private Scanner scanner = new Scanner(System.in);
+
+    public List<String> gather() {
+
+        return getName();
+    }
+
+    private List<String> getName() {
+        List<String> nickNames = new ArrayList<>();
+
+        System.out.println("enter the name : ");
+        String name = scanner.nextLine().trim();
+        while (!name.isEmpty()){
+            nickNames.add(name);
+            System.out.println("enter the name : ");
+            name = scanner.nextLine().trim();
+        }
+    return nickNames;
+    }
+}
