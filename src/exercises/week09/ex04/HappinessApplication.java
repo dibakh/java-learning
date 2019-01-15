022eb2a85dc61ca4ce853cc262c5214a917ae01d
DIBA <diba.kheirkhah@gmail.com>
2019-01-15 12:52:29 +0100

    displaying five first happy countries using stream

---
+++ src/exercises/week09/ex04/HappinessApplication.java
@@ -0,0 +1,17 @@
+package exercises.week09.ex04;
+
+import exercises.week09.ex02.FileReader;
+
+import java.util.Comparator;
+
+public class HappinessApplication {
+    public static void main(String[] args) {
+        new FileReader().asStream("exercises/week09/ex04/world-happiness-2017.csv")
+                .skip(1)
+                .map(e -> e.split(";"))
+                .map(e -> new HappinessRecord(e[0], Integer.valueOf(e[1]), Double.valueOf(e[2])))
+                .sorted(Comparator.comparing(HappinessRecord::getRank))
+                .limit(5)
+                .forEach(e -> System.out.println("Rank: "+e.getRank()+" | Country: "+e.getCountry()+" | Score: "+e.getScore()));
+    }
+}
