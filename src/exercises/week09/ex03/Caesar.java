681a76a3a82136d3637016f4946b5cea5ef979ff
DIBA <diba.kheirkhah@gmail.com>
2019-01-15 12:18:28 +0100

    caesar algorithm(refactor numbers to constant

--- src/exercises/week09/ex03/Caesar.java
+++ src/exercises/week09/ex03/Caesar.java
@@ -5,8 +5,11 @@ import java.util.stream.Stream;
 
 public class Caesar {
 
-    public String cipher(String text, Integer key) {
+    private static final int NUMBER_OF_LETTERS = 26;
+    public static final int SMALL_A_ASCII_CODE = 97;
+    public static final int Big_A_Ascii_Code = 65;
 
+    public String cipher(String text, Integer key) {
         return Stream.of(text.split(""))
                 .map(s -> s.charAt(0))
                 .map(s -> letterShifter(s, key))
@@ -23,9 +26,9 @@ public class Caesar {
     private String shift(Character letter, Integer key) {
         int shiftedAsciiCode = letter + key;
         if (Character.isUpperCase(letter)) {
-            shiftedAsciiCode = (shiftedAsciiCode - 65) % 26 + 65;
+            shiftedAsciiCode = (shiftedAsciiCode - Big_A_Ascii_Code) % NUMBER_OF_LETTERS + Big_A_Ascii_Code;
         } else {
-            shiftedAsciiCode = (shiftedAsciiCode - 97) % 26 + 97;
+            shiftedAsciiCode = (shiftedAsciiCode - SMALL_A_ASCII_CODE) % NUMBER_OF_LETTERS + SMALL_A_ASCII_CODE;
         }
         Character cipher = (char) shiftedAsciiCode;
         return cipher.toString();
