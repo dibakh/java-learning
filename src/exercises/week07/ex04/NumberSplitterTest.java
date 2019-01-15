8c46795e79b579411270a202ac565e73b4fee7ae
DIBA <diba.kheirkhah@gmail.com>
2019-01-15 06:43:48 +0100

    test an Integer number

---
+++ src/exercises/week07/ex04/NumberSplitterTest.java
@@ -0,0 +1,34 @@
+package exercises.week07.ex04;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+class NumberSplitterTest {
+
+    @Test
+    void fourDigitNumber() {
+        Integer number=4653;
+
+        NumberSplitter numberSplitter = new NumberSplitter();
+
+        List<Integer> answer = numberSplitter.splitNumber(number);
+        List<Integer> expect = Arrays.asList(4,6,5,3);
+
+        Assertions.assertEquals(expect,answer);
+    }
+
+    @Test
+    void sixDigitNumber() {
+        Integer number=193822;
+
+        NumberSplitter numberSplitter = new NumberSplitter();
+
+        List<Integer> answer = numberSplitter.splitNumber(number);
+        List<Integer> expect = Arrays.asList(1,9,3,8,2,2);
+
+        Assertions.assertEquals(expect,answer);
+    }
+}
