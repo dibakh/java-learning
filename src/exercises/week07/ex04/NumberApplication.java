a8f1c541aefacd6a915a957c9e47dc3780b423f8
DIBA <diba.kheirkhah@gmail.com>
2019-01-15 06:39:53 +0100

    main application

---
+++ src/exercises/week07/ex04/NumberApplication.java
@@ -0,0 +1,10 @@
+package exercises.week07.ex04;
+
+public class NumberApplication {
+    public static void main(String[] args) {
+
+        Number number = new Number();
+        number.recieveNumer();
+
+    }
+}
