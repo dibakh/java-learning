de043f10134fcaceee74a20b92ac244428948570
DIBA <diba.kheirkhah@gmail.com>
2019-01-15 10:17:18 +0100

    test symbols, upper and lower cases

--- src/exercises/week09/ex03/CaesarTest.java
+++ src/exercises/week09/ex03/CaesarTest.java
@@ -1,12 +1,30 @@
 package exercises.week09.ex03;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CaesarTest {
+    private Caesar caesar = new Caesar();
+    @Test
+    void allBigLettersThroughStream(){
+        String result = caesar.cipher("HELLO", 1);
+        String expected = "IFMMP";
+        Assertions.assertEquals(expected,result);
+    }
 
     @Test
-    void cipher() {
+    void allSmallWithSymbolThroughStream(){
+        String result = caesar.cipher("hello, world", 13);
+        String expected = "uryyb, jbeyq";
+        Assertions.assertEquals(expected,result);
     }
+
+    @Test
+    void testEncryptThroughStream() {
+        String message = "be sure to drink your Ovaltine";
+        String encryptedMessage = caesar.cipher(message, 13);
+        String expected = "or fher gb qevax lbhe Binygvar";
+        Assertions.assertEquals(expected, encryptedMessage);
+    }
+
 }
