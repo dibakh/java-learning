3c14755bfd931e1a3aa7af004a61f1aceaf77b1e
DIBA <diba.kheirkhah@gmail.com>
2019-01-15 06:34:03 +0100

    find biggest number, using Collection.sort

---
+++ src/exercises/week07/ex03/BiggestNumber.java
@@ -0,0 +1,14 @@
+package exercises.week07.ex03;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BiggestNumber {
+    public Integer findBiggestNumber(List<Integer> numbers) {
+        Collections.sort(numbers);
+        Integer lastPosition = numbers.size() - 1;
+
+        return numbers.get(lastPosition);
+
+    }
+}
