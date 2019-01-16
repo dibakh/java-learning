cb92ae3045bcabc1eccc8206fac6e6a1f0796799
DIBA <diba.kheirkhah@gmail.com>
2019-01-16 19:54:47 +0100

    display customized film titles|update: refactor methods

--- src/challenge/directions/North.java
+++
@@ -1,4 +0,0 @@
-package challenge.directions;
-
-public class North {
-}
--- src/exercises/week09/ex05/FilmSummarizer.java
+++ src/exercises/week09/ex05/FilmSummarizer.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FilmSummarizer {
-
     List<Film> films = new FilmReader().getFilms();
 
     public void analyseData() {
@@ -18,28 +17,49 @@ public class FilmSummarizer {
         top3MostPopularVotedMoreThan10MilionSrtedByRate();
     }
 
-    private void top3MostPopularVotedMoreThan10MilionSrtedByRate() {
+    private void top3HighestRaitingSortedByRating() {
         List<String> collect = films.stream()
-                .filter(film -> film.getVoteCount() > 10000)
                 .sorted(Comparator.comparing(Film::getScore).reversed())
                 .limit(3)
                 .map(film -> film.getTitle())
                 .collect(Collectors.toList());
 
-        System.out.println("\nTop 3 most popular than voted more than 10 milion times and sorted by rate");
+        System.out.println("\nTop 3 with highest rating, sorted by rating:");
         System.out.println(collect);
     }
 
-    private void top5MostRatedEarnedMore10MilionLongerThan2Hours() {
+    private void top3longer3hoursSortedByRate() {
         List<String> collect = films.stream()
-                .filter(film -> film.getRuntime() > 120)
-                .filter(film -> film.getRevenue() > 100000000)
-                .sorted((Comparator.comparing(Film::getScore).reversed()))
-                .limit(5)
+                .filter(film -> film.getRuntime() > 180)
+                .sorted(Comparator.comparing(Film::getScore).reversed())
+                .limit(3)
                 .map(film -> film.getTitle())
                 .collect(Collectors.toList());
 
-        System.out.println("\nTop 5 most rated earned more than 10 milion longer that 2 hours");
+        System.out.println("\nTop 3 with highest rating longer than 3 hours, sorted by rating:");
+        System.out.println(collect);
+    }
+
+    private void top4MoreExpensiveSortedByBudget() {
+        List<String> collect = films.stream()
+                .sorted(Comparator.comparing(Film::getBudget).reversed())
+                .limit(4)
+                .map(film -> film.getTitle())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 4 most expensive, sorted by budget:");
+        System.out.println(collect);
+    }
+
+    private void top4mostExpensiveShorterThan90SortedByBudget() {
+        List<String> collect = films.stream()
+                .filter(film -> film.getRuntime() < 90)
+                .sorted(Comparator.comparing(Film::getBudget).reversed())
+                .limit(4)
+                .map(film -> film.getTitle())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 4 most expensive shorter than 90 minutes, sorted by budget:");
         System.out.println(collect);
     }
 
@@ -56,49 +76,28 @@ public class FilmSummarizer {
         System.out.println(collect);
     }
 
-    private void top4mostExpensiveShorterThan90SortedByBudget() {
+    private void top5MostRatedEarnedMore10MilionLongerThan2Hours() {
         List<String> collect = films.stream()
-                .filter(film -> film.getRuntime() < 90)
-                .sorted(Comparator.comparing(Film::getBudget).reversed())
-                .limit(4)
+                .filter(film -> film.getRuntime() > 120)
+                .filter(film -> film.getRevenue() > 100000000)
+                .sorted((Comparator.comparing(Film::getScore).reversed()))
+                .limit(5)
                 .map(film -> film.getTitle())
                 .collect(Collectors.toList());
 
-        System.out.println("\nTop 4 most expensive shorter than 90 minutes, sorted by budget:");
+        System.out.println("\nTop 5 most rated earned more than 10 milion longer that 2 hours");
         System.out.println(collect);
     }
 
-    private void top4MoreExpensiveSortedByBudget() {
+    private void top3MostPopularVotedMoreThan10MilionSrtedByRate() {
         List<String> collect = films.stream()
-                .sorted(Comparator.comparing(Film::getBudget).reversed())
-                .limit(4)
-                .map(film -> film.getTitle())
-                .collect(Collectors.toList());
-
-        System.out.println("\nTop 4 most expensive, sorted by budget:");
-        System.out.println(collect);
-    }
-
-    private void top3longer3hoursSortedByRate() {
-        List<String> collect = films.stream()
-                .filter(film -> film.getRuntime() > 180)
+                .filter(film -> film.getVoteCount() > 10000)
                 .sorted(Comparator.comparing(Film::getScore).reversed())
                 .limit(3)
                 .map(film -> film.getTitle())
                 .collect(Collectors.toList());
 
-        System.out.println("\nTop 3 with highest rating longer than 3 hours, sorted by rating:");
-        System.out.println(collect);
-    }
-
-    private void top3HighestRaitingSortedByRating() {
-        List<String> collect = films.stream()
-                .sorted(Comparator.comparing(Film::getScore).reversed())
-                .limit(3)
-                .map(film -> film.getTitle())
-                .collect(Collectors.toList());
-
-        System.out.println("\nTop 3 with highest rating, sorted by rating:");
+        System.out.println("\nTop 3 most popular than voted more than 10 milion times and sorted by rate");
         System.out.println(collect);
     }
 }
