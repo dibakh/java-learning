8eb8cdf52e3914f12b11c36912c7f05d509e4b6b
DIBA <diba.kheirkhah@gmail.com>
2019-01-17 18:38:01 +0100

    updated: clean code

--- src/exercises/week09/ex02/StreamsTest.java
+++ src/exercises/week09/ex02/StreamsTest.java
@@ -6,23 +6,24 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StreamsTest {
-    private FileReader reader = new FileReader();
-    private Stream<String> names = getNames();
+    private List<String> names = new FileReader()
+            .asList("exercises/week09/ex02/names.txt");
 
     @Test
     void namesShorterThanfourChar() {
-        List<String> result = names.filter(e -> e.length() < 4)
+        List<String> result = names.stream()
+                .filter(e -> e.length() < 4)
                 .collect(Collectors.toList());
         List<String> expected = Arrays.asList("El");
         Assertions.assertEquals(expected, result);
     }
 
     @Test
-    void endWithm() {
-        List<String> result = names.filter(e -> e.endsWith("m"))
+    void endWithM() {
+        List<String> result = names.stream()
+                .filter(e -> e.endsWith("m"))
                 .map(e -> e.toUpperCase())
                 .collect(Collectors.toList());
         List<String> expected = Arrays.asList("HOSAM", "TAMMAM");
@@ -31,7 +32,8 @@ public class StreamsTest {
 
     @Test
     void containsEorR() {
-        List<String> result = names.filter(e -> e.contains("e"))
+        List<String> result = names.stream()
+                .filter(e -> e.contains("e"))
                 .filter(e -> e.contains("r"))
                 .collect(Collectors.toList());
         List<String> expected = Arrays.asList("Mehran", "Norbert", "Serife");
@@ -40,20 +42,14 @@ public class StreamsTest {
 
     @Test
     void exaclyFourCharLength() {
-        List<String> result = names.filter(e -> e.length() == 4)
+        List<String> result = names.stream()
+                .filter(e -> e.length() == 4)
                 .map(e -> e.toLowerCase())
                 .filter(e -> e.contains("a"))
                 .filter(e -> e.contains("m"))
                 .map(e -> e.replaceAll("a", "aa"))
                 .collect(Collectors.toList());
-
         List<String> expected = Arrays.asList("maarj", "aamin", "omaar");
         Assertions.assertEquals(expected, result);
     }
-
-    private Stream<String> getNames() {
-        return reader.asStream("exercises/week09/ex02/names.txt");
-    }
-
-
 }
