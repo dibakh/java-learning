b917df763eacf0b69e7f4571ce6bd0cd38262743
DIBA <diba.kheirkhah@gmail.com>
2019-01-19 11:21:02 +0100

    define List of Vehicles

--- src/exercises/week07/ex07/Vehicles.java
+++ src/exercises/week07/ex07/Vehicles.java
@@ -1,4 +1,21 @@
 package exercises.week07.ex07;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vehicles {
+    FivePersonVehicle fivePersonVehicle = new FivePersonVehicle();
+    OnePersonVehicle onePersonVehicle = new OnePersonVehicle();
+
+    public List<Vehicle> getVehicles(Integer numberOf5PersonVehicles, Integer numberOf1PersonVehicles) {
+        List<Vehicle> transportVehicle = new ArrayList<>();
+        for (int times = 0; times < numberOf5PersonVehicles; times++) {
+            transportVehicle.add(fivePersonVehicle);
+        }
+
+        for (int times = 0; times < numberOf1PersonVehicles; times++) {
+            transportVehicle.add(onePersonVehicle);
+        }
+        return transportVehicle;
+    }
 }
