cbb380594564939a15db73e067f03d1c4cbfd3f3
dibakh <diba.kheirkhah@gmail.com>
2019-01-23 15:11:21 +0100

    data reader

--- src/exercises/week10/ex03/SmsReader.java
+++ src/exercises/week10/ex03/SmsReader.java
@@ -1,4 +1,17 @@
 package exercises.week10.ex03;
 
+import exercises.week09.ex02.FileReader;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class SmsReader {
+    public List<Sms> getLines(){
+        return new FileReader().asStream("exercises/week10/ex03/sms.csv")
+                .skip(1)
+                .map(e->e.split(";"))
+                .map(e-> new Sms(e[0],e[1]))
+                .collect(Collectors.toList());
+    }
 }
