c81f9b0c4bb6b3f69ff760eab472408264a8a52b
dibakh <diba.kheirkhah@gmail.com>
2019-01-23 15:09:47 +0100

    data

---
+++ src/exercises/week10/ex03/Sms.java
@@ -0,0 +1,19 @@
+package exercises.week10.ex03;
+
+public class SmsSummarizer {
+    private String title;
+    private String text;
+
+    public SmsSummarizer(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
---
+++ src/exercises/week10/ex03/SmsReader.java
@@ -0,0 +1,4 @@
+package exercises.week10.ex03;
+
+public class SmsReader {
+}
