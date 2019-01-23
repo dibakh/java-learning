d729b3e89824a0f28c02e366a4872a19e5227942
dibakh <diba.kheirkhah@gmail.com>
2019-01-23 12:07:44 +0100

    read data

--- src/exercises/week10/ex02/StudentReader.java
+++ src/exercises/week10/ex02/StudentReader.java
@@ -1,14 +1,13 @@
-package reflection.week09;
+package exercises.week10.ex02;
 
 import exercises.week09.ex05.FileReader;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class StudentReader {
     public List<Student> getStudents() {
-        return new FileReader().asStream("reflection/week09/students-performance.csv")
+        return new FileReader().asStream("exercises/week10/ex02/students-performance.csv")
                 .skip(1)
                 .map(e -> e.split(";"))
                 .map(e -> new Student(
