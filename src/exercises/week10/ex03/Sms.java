fef0fe187d18b804cdfa94cae1acbc6339a3e919
dibakh <diba.kheirkhah@gmail.com>
2019-01-23 15:12:22 +0100

    define attributes

--- src/exercises/week10/ex03/Sms.java
+++ src/exercises/week10/ex03/Sms.java
@@ -1,19 +1,21 @@
 package exercises.week10.ex03;
 
-public class SmsSummarizer {
+public class Sms {
     private String title;
     private String text;
 
-    public SmsSummarizer(String title, String text) {
+    public Sms(String title, String text) {
         this.title = title;
         this.text = text;
     }
 
     public String getTitle() {
+
         return title;
     }
 
     public String getText() {
+
         return text;
     }
 }
