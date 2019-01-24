712ad792175a3fd1c17b4960de4f27de7500e4b0
dibakh <diba.kheirkhah@gmail.com>
2019-01-24 16:15:33 +0100

    main application

--- src/exercises/week10/ex04/CharacterApplication.java
+++ src/exercises/week10/ex04/CharacterApplication.java
@@ -1,4 +1,28 @@
 package exercises.week10.ex04;
 
+import exercises.week10.ex04.display.CharacterDisplay;
+
 public class CharacterApplication {
+    public static void main(String[] args) {
+        CharacterDisplay display = new CharacterDisplay();
+
+        display.numberOfDeadChar();
+        display.percantageOfDeathManAndWoman();
+        display.totalCharInBook();
+        display.bookWithHighestDeath();
+        display.twoAllegiancesWithHighestDead();
+        display.whoIsDeadInBookOfHighestDead();
+        display.percantageOfNobilityChar();
+        display.bookWithMostdeadFromStarkAllegiance();
+        display.whichBookHasMostDeadFromLannister();
+        display.howManyStraksAreDead();
+        display.howManyLannisterAreDead();
+        display.anyAliveChar();
+        display.AnyCharIntroducedAndKilledInSameChapter();
+    }
 }
+
+
+
+
+
