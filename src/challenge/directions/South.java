e69c1bd865014813e2c8074fcbb858cd87b500f6
diba <diba.kheikhah@gmail.com>
2019-01-28 13:39:27 +0100

    south direction

--- src/challenge/directions/South.java
+++ src/challenge/directions/South.java
@@ -8,31 +8,20 @@ public class South implements Direction {
 
     @Override
     public String getName() {
+
         return name;
     }
 
     @Override
     public boolean defeats(Direction direction) {
 
-        return "north".equalsIgnoreCase("south");
+        return "north".equalsIgnoreCase(direction.getName());
     }
 
     @Override
     public String toString() {
+
         return "South";
     }
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        South south = (South) o;
-        return Objects.equals(name, south.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
