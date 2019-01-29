0fdec6260d9e3ec55a67974b8c24335110d6becf
diba <diba.kheirkhah@gmail.com>
2019-01-29 21:32:53 +0100

    test corner cases

--- src/exercises/week11/ex02/MillitaryTimeTest.java
+++ src/exercises/week11/ex02/MillitaryTimeTest.java
@@ -1,12 +1,20 @@
 package exercises.week11.ex02;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MillitaryTimeTest {
+    Convert convert = new Convert();
 
+    //        Have in mind that midnight is 12:00:00 AM on a 12-hour clock
     @Test
-    void toMilitaryTime() {
+    void toMilitaryTimeExample() {
+        String result = convert.toMilitaryTime("12:00:00 AM");
+        Assertions.assertEquals("00:00:00", result);
     }
+//        and 00:00:00 on a 24-hour clock.
+
+//        Also that noon is 12:00:00 PM on a 12-hour clock,
+//        and 12:00:00 on a 24-hour clock.
 }
