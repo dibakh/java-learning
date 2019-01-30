458ed63ff025646e3a50e9ea099da5ccee01b7f4
diba <diba.kheirkhah@gmail.com>
2019-01-30 18:37:49 +0100

    night extend of moment

--- src/exercises/week11/ex03/Night.java
+++ src/exercises/week11/ex03/Night.java
@@ -1,4 +1,10 @@
 package exercises.week11.ex03;
 
-public class Night {
+import java.time.LocalTime;
+
+public class Night extends Moment {
+//    For hours between 20:01 and 5:59, add at night
+    public Night() {
+        super(LocalTime.of(20, 01), LocalTime.of(5, 59), "night");
+    }
 }
