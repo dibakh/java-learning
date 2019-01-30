e35827e86dd9352e89f911c9eb4c5a8e4bfd72c9
diba <diba.kheirkhah@gmail.com>
2019-01-30 16:08:04 +0100

    mapping numbers

---
+++ src/exercises/week11/ex03/ConvertTest.java
@@ -0,0 +1,12 @@
+package exercises.week11.ex03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConvertTest {
+
+    @Test
+    void toWords() {
+    }
+}
