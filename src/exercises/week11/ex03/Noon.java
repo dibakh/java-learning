8f76c8243e076802f93bc338891fa5187dd8b53c
diba <diba.kheirkhah@gmail.com>
2019-01-30 18:38:30 +0100

    noon extend of moment

--- src/exercises/week11/ex03/Noon.java
+++ src/exercises/week11/ex03/Noon.java
@@ -1,4 +1,10 @@
 package exercises.week11.ex03;
 
-public class Noon {
+import java.time.LocalTime;
+
+public class Noon extends Moment {
+    //    At 12 o’clock, replace the whole message by noon
+    public Noon() {
+        super(LocalTime.of(12, 00), LocalTime.of(12, 00), "noon");
+    }
 }
