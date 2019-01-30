ea8d6f65fef3ee9e0e9d862f824b856efba0e2ee
diba <diba.kheirkhah@gmail.com>
2019-01-30 18:37:18 +0100

    morning extend of moment

--- src/exercises/week11/ex03/Morning.java
+++ src/exercises/week11/ex03/Morning.java
@@ -1,4 +1,10 @@
 package exercises.week11.ex03;
 
-public class Morning {
+import java.time.LocalTime;
+
+public class Morning extends Moment {
+    //    For hours between 6:00 and 11:59, say in the morning
+    public Morning() {
+        super(LocalTime.of(6, 00), LocalTime.of(6, 00), "morning");
+    }
 }
