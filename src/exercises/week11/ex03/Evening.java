31a699244b28bfc414ce3102c203bb0b4a888280
diba <diba.kheirkhah@gmail.com>
2019-01-30 18:34:44 +0100

    evening extend of moment

--- src/exercises/week11/ex03/Evening.java
+++ src/exercises/week11/ex03/Evening.java
@@ -1,4 +1,10 @@
 package exercises.week11.ex03;
 
-public class Evening {
+import java.time.LocalTime;
+
+public class Evening extends Moment {
+//    For hours between 17:01 and 20:00, add in the evening
+    public Evening() {
+        super(LocalTime.of(17,01), LocalTime.of(20,0), "evening");
+    }
 }
