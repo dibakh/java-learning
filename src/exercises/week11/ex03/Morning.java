3f7a6fe49c965212487c8a6c0451e2d82b89325c
diba <diba.kheirkhah@gmail.com>
2019-01-31 19:29:19 +0100

    implement of moment

--- src/exercises/week11/ex03/Morning.java
+++ src/exercises/week11/ex03/Morning.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 public class Morning extends Moment {
     //    For hours between 6:00 and 11:59, say in the morning
     public Morning() {
+
         super(LocalTime.of(6, 00), LocalTime.of(6, 00), "morning");
     }
 }
