8ddf48b3f5a3119ebddc60b327c331aac0152a4b
diba <diba.kheirkhah@gmail.com>
2019-01-31 19:29:06 +0100

    implement of moment

--- src/exercises/week11/ex03/Midnight.java
+++ src/exercises/week11/ex03/Midnight.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 public class Midnight extends Moment {
     //    At 24 o’clock, replace the whole message by midnight
     public Midnight() {
+
         super(LocalTime.of(0, 0), LocalTime.of(0, 0), "midnight");
     }
 }
