41f0ed588fa695e65f58e000d235188af28fd04d
diba <diba.kheirkhah@gmail.com>
2019-01-31 19:30:40 +0100

    insert / delete / replace

--- src/exercises/week11/ex04/Word.java
+++ src/exercises/week11/ex04/Word.java
@@ -1,4 +1,27 @@
 package exercises.week11.ex04;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Word {
+    public Boolean compare(String referenceWord, String currentWord) {
+//        if (currentWord.length() > referenceWord.length()) {
+//            String temp = referenceWord;
+//            referenceWord = currentWord;
+//            currentWord = temp;
+//        }
+
+        String collect = Pattern.compile("").splitAsStream(referenceWord)
+                .filter(e -> !currentWord.contains(e))
+                .collect(Collectors.joining());
+
+        if (!referenceWord.equalsIgnoreCase(currentWord) && collect.length() == 0) {
+            return false;
+        }
+        return collect.length() == 1;
+    }
+
+
 }
