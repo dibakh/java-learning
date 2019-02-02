b9c52d9bae4fbd4977653407ef3b954704452675
diba <diba.kheirkhah@gmail.com>
2019-02-02 21:33:26 +0100

    implement of Time

--- src/exercises/week11/ex03/MinBeforeAndEqual30.java
+++ src/exercises/week11/ex03/MinBeforeAndEqual30.java
@@ -1,4 +1,18 @@
 package exercises.week11.ex03;
 
-public class MinBeforeAndEqual30 {
+import java.util.Map;
+
+public class MinBeforeAndEqual30 implements Time {
+    private Map<Integer, String> numbers = Numbers.getNumbersAndWords();
+
+    @Override
+    public Boolean isInRange(Integer minute) {
+
+        return minute > 0 && minute < 31;
+    }
+
+    @Override
+    public String getHourInWord(Integer hour, Integer minute) {
+        return numbers.get(minute) + " past " + numbers.get(hour);
+    }
 }
