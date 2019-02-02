3b285182730179c2481b95b86a267bb3a9ba84fc
diba <diba.kheirkhah@gmail.com>
2019-02-02 21:33:00 +0100

    implement of Time

--- src/exercises/week11/ex03/OClock.java
+++ src/exercises/week11/ex03/OClock.java
@@ -1,4 +1,21 @@
 package exercises.week11.ex03;
 
-public class OClock {
+import java.util.Map;
+
+public class OClock implements Time {
+    private Map<Integer, String> numbers = Numbers.getNumbersAndWords();
+
+    @Override
+    public Boolean isInRange(Integer minute) {
+
+        return minute.equals(0);
+    }
+
+    @Override
+    public String getHourInWord(Integer hour, Integer minute) {
+        if (hour.equals(0) || hour.equals(24)) {
+            return "midnight";
+        }
+        return numbers.get(hour);
+    }
 }
