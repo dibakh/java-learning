4b0391912a308b5b5bd895c97fd586fadaf187e2
diba <diba.kheirkhah@gmail.com>
2019-02-05 21:31:07 +0100

    up corner cases

--- src/challenge/raiCipher/UpStrategyTest.java
+++ src/challenge/raiCipher/UpStrategyTest.java
@@ -1,12 +1,33 @@
 package challenge.raiCipher;
 
+import challenge.railFenceCipher.Up;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UpStrategyTest {
 
+    private UpStrategy upStrategy = new UpStrategy(3);
+
     @Test
     void getNext() {
+        Coordinate coordinate = new Coordinate(2, 2);
+
+        Optional<Coordinate> result = upStrategy.getNext(coordinate);
+
+        Coordinate expected = new Coordinate(3, 1);
+        assertEquals(expected, result.get());
+    }
+
+    @Test
+    void getOutOfBounds() {
+        Coordinate coordinate = new Coordinate(4, 0);
+
+        Optional<Coordinate> result = upStrategy.getNext(coordinate);
+
+        assertFalse(result.isPresent());
     }
 }
