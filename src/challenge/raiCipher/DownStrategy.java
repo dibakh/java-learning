e7717cf271916d0be038e4b2efd91e8fddd38d46
diba <diba.kheirkhah@gmail.com>
2019-02-05 21:29:57 +0100

    down direction implement of strategy

--- src/challenge/raiCipher/DownStrategy.java
+++ src/challenge/raiCipher/DownStrategy.java
@@ -1,4 +1,23 @@
 package challenge.raiCipher;
 
-public class DownStrategy {
+import java.util.Optional;
+
+public class DownStrategy implements Strategy {
+
+    private Integer numberOfRow;
+
+    public DownStrategy(Integer numberOfRow) {
+
+        this.numberOfRow = numberOfRow;
+    }
+
+    @Override
+    public Optional<Coordinate> getNext(Coordinate last) {
+        Integer row = last.getRow();
+        Integer column = last.getColumn();
+        if (row >= numberOfRow - 1) {
+            return Optional.empty();
+        }
+        return Optional.of(new Coordinate(column + 1, row + 1));
+    }
 }
