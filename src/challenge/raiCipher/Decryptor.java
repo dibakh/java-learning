f41d0ff2c255fc29b813878d8624c27e9231ced3
diba <diba.kheirkhah@gmail.com>
2019-02-05 21:37:20 +0100

    decryption logic

--- src/challenge/raiCipher/Decryptor.java
+++ src/challenge/raiCipher/Decryptor.java
@@ -1,4 +1,29 @@
 package challenge.raiCipher;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class Decryptor {
+
+    private CipherMap cipherMap = new CipherMap();
+
+    public String getDecryptCoordinates(String cipheredText, Integer numRails) {
+        List<Coordinate> decryptCoordinates = new RailCipher(cipheredText).getCoordinates(numRails);
+//        change order
+        decryptCoordinates = decryptCoordinates.stream()
+                .sorted(Comparator.comparingInt(e -> e.getRow()))
+//                .sorted(Comparator.comparingInt(e -> e.getColumn()))
+                .collect(toList());
+        Map<Coordinate, String> decryptMap = cipherMap.makeCipherMap(decryptCoordinates, cipheredText);
+        String plainText = decryptMap.entrySet().stream()
+                .sorted(Comparator.comparingInt(e -> e.getKey().getColumn()))
+                .map(e -> e.getValue())
+                .collect(joining());
+
+        return plainText;
+    }
 }
