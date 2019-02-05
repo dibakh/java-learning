13e6d5b943ef0740a62f62a23fbf598bca4625cd
diba <diba.kheirkhah@gmail.com>
2019-02-05 21:29:30 +0100

    up direction implement of strategy

--- src/challenge/raiCipher/UpStrategy.java
+++ src/challenge/raiCipher/UpStrategy.java
@@ -1,4 +1,21 @@
 package challenge.raiCipher;
 
-public class UpStrategy {
+import java.util.Optional;
+
+public class UpStrategy implements Strategy {
+    private Integer numberOfRow;
+
+    public UpStrategy(Integer numberOfRow) {
+        this.numberOfRow = numberOfRow;
+    }
+
+    @Override
+    public Optional<Coordinate> getNext(Coordinate last) {
+        Integer column = last.getColumn();
+        Integer row = last.getRow();
+        if (row <= 0) {
+            return Optional.empty();
+        }
+        return Optional.of(new Coordinate(column + 1, row - 1));
+    }
 }
