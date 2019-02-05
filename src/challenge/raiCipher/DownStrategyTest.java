b10c8f5b473895e71ca3beb61c663031e95b3f4e
diba <diba.kheirkhah@gmail.com>
2019-02-05 21:30:37 +0100

    Down corner cases

--- src/challenge/raiCipher/DownStrategyTest.java
+++ src/challenge/raiCipher/DownStrategyTest.java
@@ -1,5 +1,32 @@
+package challenge.raiCipher;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DownStrategyTest {
 
+    private DownStrategy downStrategy = new DownStrategy(3);
+
+    @Test
+    void getNext() {
+        Coordinate coordinate = new Coordinate(0, 0);
+
+        Optional<Coordinate> result = downStrategy.getNext(coordinate);
+
+        Coordinate expected = new Coordinate(1, 1);
+        assertEquals(expected, result.get());
+    }
+
+    @Test
+    void getNextOutOfBounds() {
+        Coordinate coordinate = new Coordinate(2, 2);
+
+        Optional<Coordinate> result = downStrategy.getNext(coordinate);
+
+        assertFalse(result.isPresent());
+    }
 }
