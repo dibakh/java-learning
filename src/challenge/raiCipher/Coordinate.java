abdf7de6b20f73d5ac6bfa7ca304232ad1e29187
diba <diba.kheirkhah@gmail.com>
2019-02-05 21:34:23 +0100

    coordinate class

--- src/challenge/raiCipher/Coordinate.java
+++ src/challenge/raiCipher/Coordinate.java
@@ -1,4 +1,35 @@
 package challenge.raiCipher;
 
+import java.util.Objects;
+
 public class Coordinate {
+    Integer column;
+    Integer row;
+
+    public Coordinate(Integer column, Integer row) {
+        this.column = column;
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return column.equals(that.column) &&
+                row.equals(that.row);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(column, row);
+    }
 }
