4168647da5e0c617417e8013e8d2907f0d236ec5
diba <diba.kheirkhah@gmail.com>
2019-02-06 12:37:40 +0100

    decryptor

--- src/challenge/raiCipher/Decryptor.java
+++ src/challenge/raiCipher/Decryptor.java
@@ -1,29 +1,24 @@
 package challenge.raiCipher;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class Decryptor {
 
-    private CipherMap cipherMap = new CipherMap();
-
     public String getDecryptCoordinates(String cipheredText, Integer numRails) {
         List<Coordinate> decryptCoordinates = new RailCipher(cipheredText).getCoordinates(numRails);
-//        change order
         decryptCoordinates = decryptCoordinates.stream()
-                .sorted(Comparator.comparingInt(e -> e.getRow()))
-//                .sorted(Comparator.comparingInt(e -> e.getColumn()))
+                .sorted(Comparator.comparingInt(Coordinate::getRow))
                 .collect(toList());
-        Map<Coordinate, String> decryptMap = cipherMap.makeCipherMap(decryptCoordinates, cipheredText);
-        String plainText = decryptMap.entrySet().stream()
-                .sorted(Comparator.comparingInt(e -> e.getKey().getColumn()))
-                .map(e -> e.getValue())
-                .collect(joining());
+        Map<Coordinate, String> decryptMap = CipherMapCreator.makeCipherMap(decryptCoordinates, cipheredText);
 
-        return plainText;
+        return decryptMap.entrySet().stream()
+                .sorted(Comparator.comparingInt(e -> e.getKey().getColumn()))
+                .map(Map.Entry::getValue)
+                .collect(joining());
     }
 }
