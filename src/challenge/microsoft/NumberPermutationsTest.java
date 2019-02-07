a0a91f7c578872f39d182c704a924a82f844757e
diba <diba.kheirkhah@gmail.com>
2019-02-07 15:52:44 +0100

    test

--- src/challenge/microsoft/NumberPermutationsTest.java
+++ src/challenge/microsoft/NumberPermutationsTest.java
@@ -1,13 +1,9 @@
-package challenge.Microsoft;
+package challenge.microsoft;
 
-import challenge.findFirstSmaller.NumberSplitter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class NumberPermutationsTest {
 
