e1b4e1059294c6d48af6c01a56cb1b1a2ad87248
diba <diba.kheirkhah@gmail.com>
2019-02-07 15:43:50 +0100

    splitter corner cases

--- src/challenge/findFirstSmaller/NumberSplitterTest.java
+++ src/challenge/findFirstSmaller/NumberSplitterTest.java
@@ -1,6 +1,9 @@
 package challenge.findFirstSmaller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +11,20 @@ class NumberSplitterTest {
 
     @Test
     void split() {
+        NumberSplitter numberSplitter = new NumberSplitter();
+        Stack<Integer> actual = numberSplitter.toStack(123);
+        Stack<Integer> expected = getValues();
+
+        Assertions.assertEquals(expected, actual);
+
     }
+
+    private Stack<Integer> getValues() {
+        Stack<Integer> values = new Stack<>();
+        values.push(1);
+        values.push(2);
+        values.push(3);
+        return values;
+    }
+
 }
