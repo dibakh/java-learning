c00f8afad51de86bfda567b0771650b8588920af
diba <diba.kheirkhah@gmail.com>
2019-02-07 15:37:10 +0100

    logic

--- src/challenge/editWords/Word.java
+++ src/challenge/editWords/Word.java
@@ -22,11 +22,3 @@ public class Word {
         return collect.length() == 1;
     }
 }
-//Challenge: There are three types of edits that can be performed on strings: insert a character,
-//remove a character, or replace a character. Given two strings, write a function to check if they are
-//one edit (or zero edits) away.
-//Example
-//pale,  ple-> true
-//pale,  pales -> true
-//pale, bale -> true
-//pale, bake -> false
