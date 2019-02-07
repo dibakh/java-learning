dc7761964a0b076db7ecf8e44fef466572b97516
diba <diba.kheirkhah@gmail.com>
2019-02-07 15:43:16 +0100

    split the number

--- src/challenge/findFirstSmaller/NumberSplitter.java
+++ src/challenge/findFirstSmaller/NumberSplitter.java
@@ -1,4 +1,36 @@
 package challenge.findFirstSmaller;
 
-public class NumberrSplitter {
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class NumberSplitter {
+
+    public Stack<Integer> toStack(Integer number) {
+        Stack<Integer> stack = new Stack<>();
+        Stream.of(number.toString().split(""))
+                .map(Integer::valueOf)
+                .forEach(stack::push);
+        return stack;
+    }
+
+    public ArrayList<Integer> toArrayList(Integer number) {
+        ArrayList<Integer> digits = new ArrayList<>();
+        Stream.of(number.toString().split(""))
+                .map(Integer::valueOf)
+                .forEach(digits::add);
+        return digits;
+    }
+
+    public Integer[] toArray(Integer number) {
+        List<Integer> list=toArrayList(number);
+
+        int size = list.size();
+
+        Integer digitArray[]=new Integer[(int) size];
+        for (int position = 0; position < size; position++) {
+            digitArray[position]=list.get(position);
+        }
+        return digitArray;
+    }
 }
