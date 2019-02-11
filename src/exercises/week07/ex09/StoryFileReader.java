02cb3ad2bba2a7bc5c67c420db1af73172245eae
diba <diba.kheirkhah@gmail.com>
2019-02-12 00:12:55 +0100

    reading word of story

--- src/exercises/week07/ex09/StoryFileReader.java
+++ src/exercises/week07/ex09/StoryFileReader.java
@@ -1,4 +1,37 @@
 package exercises.week07.ex09;
 
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StoryFileReader {
+    public Set<String> getLine() {
+        return new FileReader().asStream("exercises/week07/ex09/book/alice's-adventures-in-wonderland.txt")
+                .map(String::toLowerCase)
+                .map(e -> e.replaceAll(",", " "))
+//                .map(e -> e.replaceAll("’s", " "))
+//                .map(e -> e.replaceAll("’", " "))
+                .map(e -> e.replaceAll("!", " "))
+                .map(e -> e.replaceAll("\\?", " "))
+                .map(e -> e.replaceAll(";", " "))
+                .map(e -> e.replaceAll("‘", " "))
+                .map(e -> e.replaceAll(":", " "))
+                .map(e -> e.replaceAll("#", " "))
+                .map(e -> e.replaceAll("/", " "))
+                .map(e -> e.replaceAll("-", " "))
+                .map(e -> e.replaceAll("\\.", " "))
+                .map(e -> e.replaceAll("\\*", " "))
+                .map(e -> e.replaceAll("\\(", " "))
+                .map(e -> e.replaceAll("\\)", " "))
+                .map(e -> e.replaceAll("\\[", " "))
+                .map(e -> e.replaceAll("\\]", " "))
+                .map(e -> e.replaceAll("\\“", " "))
+                .map(e -> e.replaceAll("\\”", " "))
+                .map(e -> e.replaceAll("\\]", " "))
+                .map(e -> e.split(" "))
+                .flatMap(Stream::of)
+                .collect(Collectors.toSet());
+
+    }
 }
