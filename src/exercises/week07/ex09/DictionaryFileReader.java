2cb5bf656946996c76cc9fed9b783188aad7474a
diba <diba.kheirkhah@gmail.com>
2019-02-12 00:14:03 +0100

    read words of dictionary

--- src/exercises/week07/ex09/DictionaryFileReader.java
+++ src/exercises/week07/ex09/DictionaryFileReader.java
@@ -1,4 +1,14 @@
 package exercises.week07.ex09;
 
+import exercises.week07.ex09.FileReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DictionaryFileReader {
+    public List<String> getLine(){
+        return new FileReader().asStream("exercises/week07/ex09/word/words.txt")
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+    }
 }
