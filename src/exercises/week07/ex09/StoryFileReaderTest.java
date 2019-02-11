252447641ca38b27fd627b3efd62995fd1dd853c
diba <diba.kheirkhah@gmail.com>
2019-02-12 00:13:31 +0100

    test of story reader

--- src/exercises/week07/ex09/StoryFileReaderTest.java
+++ src/exercises/week07/ex09/StoryFileReaderTest.java
@@ -1,4 +1,17 @@
 package exercises.week07.ex09;
 
-public class StoryFileReaderTest {
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+class StoryFileReaderTest {
+
+    @Test
+    void getLine() {
+        StoryFileReader storyFileReader = new StoryFileReader();
+        Set<String> lines = storyFileReader.getLine();
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
 }
