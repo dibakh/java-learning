728ad70b93fe511d583ca8721ff8b2dac72c8031
diba <diba.kheirkhah@gmail.com>
2019-02-22 15:51:57 +0100

    data

--- src/reflection/fruitshop/model/Fruit.java
+++ src/reflection/fruitshop/model/Fruit.java
@@ -1,4 +1,17 @@
 package reflection.fruitshop.model;
 
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
 public class Fruit {
+
+    private String name;
+    private long amount;
+
+    @Override
+    public String toString() {
+        return amount + " " + name;
+    }
 }
