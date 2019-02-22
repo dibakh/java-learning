1647a4292f590e39a601fc028a8535b4f34fed48
diba <diba.kheirkhah@gmail.com>
2019-02-22 15:52:59 +0100

    display

--- src/reflection/fruitshop/view/FruitCounter.java
+++ src/reflection/fruitshop/view/FruitCounter.java
@@ -1,4 +1,27 @@
 package reflection.fruitshop.view;
 
+import lombok.experimental.UtilityClass;
+import lombok.extern.java.Log;
+import reflection.fruitshop.controller.Assistant;
+import reflection.fruitshop.model.Fruit;
+
+import java.util.List;
+
+@UtilityClass
+@Log
 public class FruitCounter {
+
+    //    FruitCounter - displays the list of fruits
+
+    private List<String> fruitNames = (List<String>) FileReader.lines("reflection/fruitshop/file/delivery.txt");
+    private List<Fruit> fruits = Assistant.toFruit(fruitNames);
+
+    public void display() {
+        log.info("These are all our tasteful fruits:");
+        fruits.stream()
+                .forEach(e-> System.out.println(e.getAmount() + " "+ e.getName()));
+
+    }
+
+
 }
