a25f3b352441d4a614510419ccae4ba190876cf0
diba <diba.kheirkhah@gmail.com>
2019-02-26 13:18:04 +0100

    using spring

--- src/spring/week02/ex2-amazinghelloworld/src/main/java/at/nacs/ex2amazinghelloworld/AmazingHelloWorld.java
+++ src/spring/week02/ex2-amazinghelloworld/src/main/java/at/nacs/ex2amazinghelloworld/AmazingHelloWorld.java
@@ -1,4 +1,10 @@
 package at.nacs.ex2amazinghelloworld;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AmazingHelloWorld {
+    public String say() {
+        return "Hello" + " World";
+    }
 }
