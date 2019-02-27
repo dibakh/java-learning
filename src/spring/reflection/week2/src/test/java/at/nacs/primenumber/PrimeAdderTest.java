d004766949a9628ec08064ef39426ca51ce389c6
diba <diba.kheirkhah@gmail.com>
2019-02-27 14:53:08 +0100

    test sum of prime numbers

--- src/spring/reflection/week2/src/test/java/at/nacs/primenumber/PrimeAdderTest.java
+++ src/spring/reflection/week2/src/test/java/at/nacs/primenumber/PrimeAdderTest.java
@@ -5,28 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
-class PrimeGeneratorTest {
+class PrimeAdderTest {
 
     @Autowired
-    PrimeGenerator primeGenerator;
-
-    @Autowired
-    Primes primes;
+    PrimeAdder primeAdder;
 
     @Test
     void getNumbers() {
-        int actual = primeGenerator.getNumbers(100);
+        int actual = primeAdder.getSum(100);
         Assertions.assertEquals(24133, actual);
     }
 
-    @Test
-    void isPrime() {
-        boolean prime = primes.isPrime(3);
-        assertTrue(prime);
-    }
 
 
 }
