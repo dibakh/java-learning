dd5a68feb731ccc7d2d063afdfcd610342c01e65
diba <diba.kheirkhah@gmail.com>
2019-02-27 14:52:46 +0100

    Primes test

--- src/spring/reflection/week2/src/test/java/at/nacs/primenumber/PrimesTest.java
+++ src/spring/reflection/week2/src/test/java/at/nacs/primenumber/PrimesTest.java
@@ -1,5 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+package at.nacs.primenumber;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class PrimesTest {
 
+    @Autowired
+    Primes primes;
+
+    @ParameterizedTest
+    @CsvSource({
+            "false , -10",
+            "false , 0",
+            "false , 1",
+            "true , 2",
+            "true , 3",
+            "true , 179",
+            "false , 100",
+    })
+    void testCounterBorad(boolean expected, int number) {
+        boolean actual = primes.isPrime(number);
+        Assertions.assertEquals(expected, actual);
+    }
 }
