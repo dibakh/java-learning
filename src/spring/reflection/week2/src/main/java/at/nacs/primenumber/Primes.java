27330bd1ad8a05ca8cc1b4ea8f5bd043d97f6843
diba <diba.kheirkhah@gmail.com>
2019-02-27 14:47:01 +0100

    check if the number prime is

--- src/spring/reflection/week2/src/main/java/at/nacs/primenumber/Primes.java
+++ src/spring/reflection/week2/src/main/java/at/nacs/primenumber/Primes.java
@@ -1,4 +1,18 @@
 package at.nacs.primenumber;
 
+import lombok.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.IntStream;
+
+@Component
+@Value
 public class Primes {
+    public boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        return IntStream.rangeClosed(2, number / 2)
+                .noneMatch(i -> number % i == 0);
+    }
 }
